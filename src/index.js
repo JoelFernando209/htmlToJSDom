793a46a978aca0919ef555fdf4751d17de14af17
Joel Fernando Dominguez Tapia <joeldomingueztapia@gmail.com>
2020-12-27 22:15:25 -0800

    Added function to include formatted HTML in InputBoard

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './scss/main.scss';
 import { codeMirrorCommonConfig } from './js/config/codeMirrorCommonConfig.js';
 import { createProcess } from './js/general/generalConfig.js';
 import { getTextEditorValue } from './js/general/codemirrorComponents.js';
-import { deleteComments } from './js/htmlManipulation/phasesComponents.js';
+import { deleteComments, putOutputInBoard } from './js/htmlManipulation/phasesComponents.js';
 
 const textEditorHtml = CodeMirror.fromTextArea(document.querySelector('.boxInput--html'), Object.assign({
   mode: 'xml',
@@ -19,6 +19,7 @@ const textEditorJs = CodeMirror.fromTextArea(document.querySelector('.boxInput--
 
 const htmlConvertProcess = createProcess(
   [
+    putOutputInBoard,
     deleteComments,
     getTextEditorValue
   ],
